abe1d70be2c532b7b0b5daaf18a614fc427f8eb9
Okamitah <loneokami03@gmail.com>
2025-02-27 17:12:25 +0000

    Started the matrix cli calculator

---
+++ python/src/linear_algebra/operations.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+def separate_matrix(expression):
+    return
+
+print("\nWelcome to my matrix calculator!\n")
+print("Allowed operators: +, -, * and /\n")
+exp = ""
+while exp != "exit":
+    exp = input("> ")
+    print()
