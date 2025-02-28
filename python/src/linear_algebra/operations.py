076500206a28de1b97476a23af44386bdbf4cc8b
Okamitah <loneokami03@gmail.com>
2025-02-28 15:05:16 +0000

    Made operations between matrices

--- python/src/linear_algebra/operations.py
+++ python/src/linear_algebra/operations.py
@@ -8,7 +8,11 @@ def separate_matrix(expression):
             expression[index] = vector.split(',')
     else:
         expression = expression.split(',')
-    return np.array(expression)
+    arr = np.array(expression)
+    for i in range(len(expression)):
+        for j in range(len(expression[0])):
+            expression[i][j] = float(expression[i][j])
+    return arr
 
 operations_map = {
     '+': lambda x,y: x+y,
@@ -17,7 +21,7 @@ operations_map = {
     '*': lambda x,y: x*y,
     '**': lambda x,y: x+y,
     '..': lambda x,y: x+y,
-    '/': lambda x,y: x+y,
+    '/': lambda x,y: x/y,
 }
 
 def split_op(expression, operations):
@@ -31,16 +35,35 @@ def split_op(expression, operations):
     expression_list.append(expression[sep:])
     return expression_list
 
-def separate_operation(expression):
+def separate_expression(expression):
     expression = expression.replace(' ','')
-    expression = expression.split('+')
-    return expression
+    expr = split_op(expression,('+','-'))
+    for index, element in enumerate(expr):
+        if any(el in element for el in ['*','/']):
+            expr[index] = split_op(element, ('*','/'))
+    return expr
 
-print(separate_matrix("1, 5  ,2 ; 2,4,8")) 
+def calculate_ls(expression):
+    separated_expression = separate_expression(expression)
+    for index in range(1,len(separated_expression),2):
+        mat1 = separate_matrix(separated_expression[index-1])
+        mat2 = separate_matrix(separated_expression[index+1])
+        separated_expression[index+1] = operations_map[separated_expression[index]](mat1,mat2)
+    return separated_expression[-1]
+
+def calculate(expression_list):
+    for lst in expression_list:
+        if type(lst) == list:
+            calculate_ls(lst)
+    return calculate_ls(expression_list)
+
+print(f"Separate matrix:{separate_matrix('1, 5  ,2 ; 2,4,8')}") 
+print(f"Separate expression:{separate_expression('1, 5  ,2 ; 2,4*5,8 + 2,2,2;4,1*4,4')}") 
+print(f"Separate expression:{calculate('1, 5  ,2 ; 2,4*5,8 + 2,2,2;4,1*4,4')}") 
 
 print("\nWelcome to my matrix calculator!\n")
 print("Allowed operators: +, -, * and /\n")
 exp = ""
-while exp != "exit":
+while exp != "e":
     exp = input("> ")
-    print(separate_matrix(exp))
+    print(calculate_ls(exp))
