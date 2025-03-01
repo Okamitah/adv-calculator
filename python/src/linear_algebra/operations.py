9ea99806128a5a96b318584d4711c3bc52d41ca4
Okamitah <loneokami03@gmail.com>
2025-03-01 08:06:43 +0000

    Fixed using up to 3 matrices at the same time

--- python/src/linear_algebra/operations.py
+++ python/src/linear_algebra/operations.py
@@ -1,17 +1,14 @@
 import numpy as np 
 
 def separate_matrix(expression):
-    expression = expression.replace(' ','')
-    if ';' in expression:
-        expression = expression.split(';')
-        for index,vector in enumerate(expression):
-            expression[index] = vector.split(',')
+    if type(expression)==str:
+        if ';' in expression:
+            expr = [row.split(',') for row in expression.split(';')]
+        else:
+            expr = [expression.split(',')]  
     else:
-        expression = expression.split(',')
-    arr = np.array(expression)
-    for i in range(len(expression)):
-        for j in range(len(expression[0])):
-            expression[i][j] = float(expression[i][j])
+        expr = expression
+    arr = np.array(expr, dtype=float)
     return arr
 
 operations_map = {
@@ -24,46 +21,55 @@ operations_map = {
     '/': lambda x,y: x/y,
 }
 
-def split_op(expression, operations):
+def split_op(expression:str, operations) -> list:
+
+    expression = expression.replace(' ', '')
     expression_list = []
     sep = 0
     for index, character in enumerate(expression):
         if character in operations:
-            expression_list.append(expression[sep:index-1])
+            expression_list.append(expression[sep:index])
             expression_list.append(expression[index])
             sep = index+1 
     expression_list.append(expression[sep:])
     return expression_list
 
-def separate_expression(expression):
-    expression = expression.replace(' ','')
-    expr = split_op(expression,('+','-'))
-    for index, element in enumerate(expr):
-        if any(el in element for el in ['*','/']):
-            expr[index] = split_op(element, ('*','/'))
+def separate_expression(expression:str) -> list:
+    operations = ('*','/','+','-')
+    expr = split_op(expression,operations)
     return expr
 
-def calculate_ls(expression):
-    separated_expression = separate_expression(expression)
-    for index in range(1,len(separated_expression),2):
-        mat1 = separate_matrix(separated_expression[index-1])
-        mat2 = separate_matrix(separated_expression[index+1])
-        separated_expression[index+1] = operations_map[separated_expression[index]](mat1,mat2)
-    return separated_expression[-1]
+def calculate(expression) -> float:
+    expression = separate_expression(expression)
+    addsub = ('+','-')
+    muldiv = ('*','/')
+    calc = []
+    for index in range(1,len(expression),2):
+        if expression[index] in addsub:
+            calc.append(expression[index-1])
+            calc.append(expression[index])
+        if expression[index] in muldiv:
+            mat1 = separate_matrix(expression[index-1])
+            mat2 = separate_matrix(expression[index+1])
+            calc.append(operations_map[expression[index]](mat1,mat2))
+    if expression[-2] in addsub:
+        calc.append(expression[-1])
+    print(f"Calc: {calc}")
+    print(f"expression: {expression}")
+    for index in range(1,len(calc),2):
+        mat1 = separate_matrix(calc[index-1])
+        mat2 = separate_matrix(calc[index+1])
+        calc[index+1] = operations_map[calc[index]](mat1,mat2)
+    return calc[-1]
 
-def calculate(expression_list):
-    for lst in expression_list:
-        if type(lst) == list:
-            calculate_ls(lst)
-    return calculate_ls(expression_list)
 
 print(f"Separate matrix:{separate_matrix('1, 5  ,2 ; 2,4,8')}") 
 print(f"Separate expression:{separate_expression('1, 5  ,2 ; 2,4*5,8 + 2,2,2;4,1*4,4')}") 
-print(f"Separate expression:{calculate('1, 5  ,2 ; 2,4*5,8 + 2,2,2;4,1*4,4')}") 
+print(f"calculate expression:{calculate('1, 4,2+5,8,3 * 3,3,3')}") 
 
 print("\nWelcome to my matrix calculator!\n")
 print("Allowed operators: +, -, * and /\n")
 exp = ""
 while exp != "e":
     exp = input("> ")
-    print(calculate_ls(exp))
+    print(calculate(exp))
