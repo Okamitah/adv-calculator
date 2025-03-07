bcec4b099d9676e676fdc965ef4dbfab7365c5dc
Okamitah <179641048+Okamitah@users.noreply.github.com>
2025-03-07 13:10:34 +0000

    Made a function that calculates whole expression with only '+' operator

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,12 +1,14 @@
 use std::{io, collections::HashMap};
 
 fn main() {
-    make_operation();
+    println!("Welcome to my basic calculator!");
+    //make_operation();
+    add_whole_expression_at_once();
 }
 
 fn make_operation() {
 
-    let mut map: HashMap<&str, Box<dyn Fn(i32, i32) -> i32>> = HashMap::new();
+    let mut map: HashMap<&str, Box<dyn Fn(f64, f64) -> f64>> = HashMap::new();
 
     map.insert("+", Box::new(|x,y| x+y));
     map.insert("-", Box::new(|x,y| x-y));
@@ -17,31 +19,23 @@ fn make_operation() {
     let mut input2 = String::new();
     let mut input3 = String::new();
 
-    let mut a = 0;
-    let mut b = 0;
+    let mut a:f64 = 0.0;
+    let mut b:f64 = 0.0;
 
     println!("Please enter the first int: ");
     io::stdin().read_line(&mut input1).expect("Couldn't read the line");
     let input1 = input1.trim();
-    match input1.parse::<i32>() {
-        Ok(num) => {
-            a = num;
-            println!("Your number is: {}", a);
-        }
-        Err(_) => {
-            println!("Invalid input1");
-        }
+    match input1.parse::<f64>() {
+        Ok(num) => a = num,
+        Err(_) => println!("Invalid input1"),
     }
 
     println!("Enter the second nbr: ");
     io::stdin().read_line(&mut input2).expect("Couldn't read the line");
     let input2 = input2.trim();
-    match input2.parse::<i32>() {
-        Ok(num) => {
-            b = num;
-            println!("Your 2nd number is: {}", b);
-        }
-        Err(_) => {println!("Invalid input")}
+    match input2.parse::<f64>() {
+        Ok(num) => b = num, 
+        Err(_) => println!("Invalid input"),
     }
     println!("Enter which operation: ");
     io::stdin().read_line(&mut input3).expect("Couldn't read the operation");
@@ -54,3 +48,19 @@ fn make_operation() {
     }
 }
 
+fn add_whole_expression_at_once() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Couldn't read the expression");
+    let input: String = input.chars().filter(|c| !c.is_whitespace()).collect();
+    let result: Vec<&str> = input.split('+').collect();
+    println!("split res: {:?}", result);
+    let mut sum: f64 = 0.0;
+    for nbr in result.iter() {
+        sum += match nbr.parse::<f64>() {
+            Ok(num) => num,
+            Err(_) => panic!("Not a number dude"),
+        }
+    }
+    println!("sum: {}", sum)
+    
+}
---
+++ rust/cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+/home/okamitah/Projects/adv-calculator/rust/cli/target/debug/cli: /home/okamitah/Projects/adv-calculator/rust/cli/src/main.rs
